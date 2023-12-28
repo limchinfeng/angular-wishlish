81b9cd6cc0bd7a4c831725461ef366e59243a260
supershuaifeng <limchinfeng@gmail.com>
2023-12-28 18:06:37 +0800

    Done basic syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
+import { WishItem } from '../shared/models/wishitem';
+import { NgIfContext } from '@angular/common';
+
+const filters = [
+  (item : WishItem) => item,
+  (item : WishItem) => !item.isComplete,
+  (item : WishItem) => item.isComplete,
+]
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  items : WishItem[] = [
+    new WishItem('To Learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Meet Deon')
+  ];
   title = 'angularWishlist';
+
+  listFilter : any = '0';
+  get visibleItems() : WishItem[] {
+    return this.items.filter(filters[this.listFilter])
+  }
+
+
+  newWishText=""
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = "";
+  }
+
+  toggleItem(item: WishItem) {
+    item.isComplete = !item.isComplete;
+    console.log(item);
+  }
 }
