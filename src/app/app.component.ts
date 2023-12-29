ac886104ea697e05073fe5e9b6959616278494bf
supershuaifeng <limchinfeng@gmail.com>
2023-12-29 22:11:03 +0800

    issues http request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,27 @@
-import { Component, TemplateRef } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { WishItem } from '../shared/models/wishitem';
 import { EventService } from './../shared/services/EventService';
+import { WishService } from './wish.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  items : WishItem[] = [
-    new WishItem('To Learn Angular'),
-    new WishItem('Get Coffee', true),
-    new WishItem('Meet Deon')
-  ];
+export class AppComponent implements OnInit {
+  title="";
+  items! : WishItem[];
   
-  constructor(events: EventService) {
+  constructor(events: EventService, private wishService: WishService) {
     events.listen('removeWish', (wish : any) => {
       let index = this.items.indexOf(wish);
       this.items.splice(index,1);
+    })
+  }
+
+  ngOnInit(): void {
+    this.wishService.getWishes().subscribe((data: any) => {
+      this.items = data;
     })
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,12 +16,13 @@ import { WishListItemComponent } from './wish-list-item/wish-list-item.component
     WishListComponent,
     AddWishFormComponent,
     WishFilterComponent,
-    WishListItemComponent
+    WishListItemComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
