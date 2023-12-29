17666dd52568dfda21bf1f2545b8e15288ca37ca
supershuaifeng <limchinfeng@gmail.com>
2023-12-29 22:25:32 +0800

    options for http request

--- src/app/wish.service.ts
+++ src/app/wish.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { WishItem } from '../shared/models/wishitem';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,31 @@ export class WishService {
 
   constructor(private http: HttpClient) { }
 
+  private getStandardOptions(): any {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    };
+  }
+
   getWishes() {
-    return this.http.get('assets/wishes.json');
+    let options = this.getStandardOptions();
+
+    //check for specific format
+    options.params = new HttpParams({
+      fromObject: {
+        format: 'json1'
+      }
+    });
+
+    return this.http.get('assets/wishes.json', options);
+  }
+
+  private addWish(wish: WishItem) {
+    let options = this.getStandardOptions();
+
+    options.headers = options.headers.set('Authorization', 'value-need-for-authorization');
+    this.http.post('assets/wishes.json', wish, options);
   }
 }
